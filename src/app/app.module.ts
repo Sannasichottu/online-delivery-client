b186e07f80b36518df4eb4d2dd798e783b1f4c05
Sannasichottu <sannasichottu@gmail.com>
2022-04-17 21:58:33 +0530

    Order list populate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from "@angular/common/http";
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
